b28c68db143354d234ad1910f4bd0e7837a94838
Sourav Kumar <souravkumar59139@gmail.com>
2025-07-26 16:51:29 +0530

    This is the first commit of the Ai SaaS project

---
+++ client/src/pages/GenerateImages.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GenerateImages = () => {
+  return (
+    <div>
+      GenerateImage
+    </div>
+  )
+}
+
+export default GenerateImages
---
+++ client/src/pages/RemoveBackground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RemoveBackground = () => {
+  return (
+    <div>
+      Remove Background Page
+    </div>
+  )
+}
+
+export default RemoveBackground
