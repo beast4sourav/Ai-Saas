a08e15a89cb0cfc46b15441b64ca25d966bfa971
Sourav Kumar <souravkumar59139@gmail.com>
2025-07-27 22:58:55 +0530

    Added AI tools

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,8 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 
 const Hero = () => {
-
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div
@@ -20,29 +19,36 @@ const Hero = () => {
           Create amazing content <br /> with
           <span className="text-primary"> AI tools</span>{" "}
         </h1>
-        <p className="mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto
-        max-sm:text-xs text-gray-600">
+        <p
+          className="mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto
+        max-sm:text-xs text-gray-600"
+        >
           Transform your content creation with our suite of premium AI tools.
           write articles, generate images, and enhance your workflow.
         </p>
       </div>
 
       <div className="flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs">
-
-        <button onClick={() => navigate("/ai")}
-        className="bg-primary text-white px-10 py-3 rounded-lg
-        hover:scale-102 active:scale-95 transition cursor-pointer">Start creating now</button>
-        <button 
-        className="bg-white px-10 py-3 rounded-lg border
+        <button
+          onClick={() => navigate("/ai")}
+          className="bg-primary text-white px-10 py-3 rounded-lg
+        hover:scale-102 active:scale-95 transition cursor-pointer"
+        >
+          Start creating now
+        </button>
+        <button
+          className="bg-white px-10 py-3 rounded-lg border
         border-gray-300 hover:scale-102 active:scale-95 transition
-        cursor-pointer">Watch demo</button>
+        cursor-pointer"
+        >
+          Watch demo
+        </button>
       </div>
 
       <div className="flex items-center gap-4 mt-8 mx-auto text-gray-600">
-        <img src={assets.user_group} alt="" className="h-8" />Trusted by 15k+
-        people
+        <img src={assets.user_group} alt="" className="h-8" />
+        Trusted by 15k+ user
       </div>
-
     </div>
   );
 };
