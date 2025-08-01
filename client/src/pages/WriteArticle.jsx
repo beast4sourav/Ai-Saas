653805f5d7860bc7c592885bba6b6913932f01a4
Sourav Kumar <souravkumar59139@gmail.com>
2025-08-01 22:46:26 +0530

    some changes

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,7 +1,41 @@
+import { Sparkle } from "lucide-react";
 import React from "react";
 
 const WriteArticle = () => {
-  return <div>WriteArticle</div>;
+
+  const articleLength = [
+    {length:800, text:"Short (500-800 words)"},
+    {length:1200, text:"Medium (800-1200 words)"},
+    {length:1600, text:"Long (1200+ words)"},
+  ]
+  return (
+    <div
+      className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4
+  text-slate-700"
+    >
+      {/* left col */}
+      <form
+        className="w-full max-w-lg p-4 bg-white rounded-lg border
+    border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkle className="w-6 text-[#4A7AFF]" />
+          <h1 className="text-xl font-semibold">Article Configuration</h1>
+        </div>
+        <p className="mt-6 text-sm font-medium">Article Topic</p>
+        <input type="text" className="w-full p-2 px-3 mt-2 outline-none text-sm
+        rounded-md border border-gray-300"  placeholder="The future of artificial intelligence is..." required/>
+        <p className="mt-4 text-sm font-medium">Article Length</p>
+        <div>
+          {articleLength.map((item, index) => (
+            <span key={index}>{item.text}</span>
+          ) )}
+        </div>
+      </form>
+      {/* Right col */}
+      <div></div>
+    </div>
+  );
 };
 
 export default WriteArticle;
